601d52f8e1d28687086d15634e787708532c42c0
Átila Delcanton Rampazo <atilarampazo@gmail.com>
2020-02-19 16:07:13 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'f7e705f3784105820f7109a75d83c248';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong> seja bem vindo a NodeStore';
 
 module.exports = {
-    connectionString: 'mongodb://atilarampazo:ztascani1978@ds163681.mlab.com:63681/node_store',
-    sendgridKey: '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',
+    connectionString: '#',
+    sendgridKey: '#',
     pathBase: __dirname + '/uploads/'
 }
